6c845b87fd5bfc70904f8a272d312d61811504d2
KenanHuang <540453543@qq.com>
2014-09-16 18:11:53 +0800

    *

--- api/add_advices/add_advice.php
+++ api/add_advices/add_advice.php
@@ -3,6 +3,7 @@ include '../../../includes/init.php';
 $url = "http://av.jejeso.com/helper/api/add_advices/thanks.html";
 $content = file_get_contents($url);
 echo $content;
+
 $name = $_REQUEST['name'];
 $dorm = $_REQUEST['dorm'];
 $phone = $_REQUEST['phone'];
