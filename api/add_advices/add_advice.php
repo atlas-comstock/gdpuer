f27487936e7d03b0f62200ce25b3f566ec9540af
KenanHuang <540453543@qq.com>
2014-09-16 18:59:01 +0800

    add action jump

--- api/add_advices/add_advice.php
+++ api/add_advices/add_advice.php
@@ -8,5 +8,5 @@ $dorm = $_REQUEST['dorm'];
 $phone = $_REQUEST['phone'];
 $advice = $_REQUEST['advice'];
 $field_values = array( "name" => $name, "dorm" =>$dorm , "phone" =>$phone, "advice" =>$advice);  
-$db->autoExecute("advices", $field_values); 
+$db->autoExecute("advices", $field_values);  
 ?>
