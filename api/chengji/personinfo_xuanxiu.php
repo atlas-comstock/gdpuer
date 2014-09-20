ef04c593a54e9fa5796010175ab57c2cf3cae895
YongHao <christopherwuy@gamil.com>
2014-09-20 09:17:22 +0800

    connan's fix of xuanxiu

--- api/chengji/personinfo_xuanxiu.php
+++ api/chengji/personinfo_xuanxiu.php
@@ -8,9 +8,9 @@ $xh = $_GET['xh'];
 $pw = $_GET['pw'];
 
 //提交账号和密码，身份模拟登陆
-$post_fields 	= '__VIEWSTATE=dDwtNjg3Njk1NzQ3O3Q8O2w8aTwxPjs%2BO2w8dDw7bDxpPDg%2BO2k8MTM%2BO2k8MTU%2BOz47bDx0PHA8O3A8bDxvbmNsaWNrOz47bDx3aW5kb3cuY2xvc2UoKVw7Oz4%2BPjs7Pjt0PHA8bDxWaXNpYmxlOz47bDxvPGY%2BOz4%2BOzs%2BO3Q8cDxsPFZpc2libGU7PjtsPG88Zj47Pj47Oz47Pj47Pj47bDxpbWdETDtpbWdUQzs%2BPvpW9bNHRO98aj%2BzEmn77FHqeOjK&tbYHM='.$xh.'&tbPSW='.$pw.'&ddlSF=%D1%A7%C9%FA&imgDL.x=28&imgDL.y=19';
+$post_fields 	= '__VIEWSTATE=dDwxOTA0NTQ3NDgwO3Q8O2w8aTwxPjs%2BO2w8dDw7bDxpPDg%2BO2k8MTM%2BO2k8MTU%2BOz47bDx0PHA8O3A8bDxvbmNsaWNrOz47bDx3aW5kb3cuY2xvc2UoKVw7Oz4%2BPjs7Pjt0PHA8bDxWaXNpYmxlOz47bDxvPGY%2BOz4%2BOzs%2BO3Q8cDxsPFZpc2libGU7PjtsPG88Zj47Pj47Oz47Pj47Pj47bDxpbWdETDtpbWdUQzs%2BPjS%2Figxoe%2FD2rIfDraki%2BD1Sdxcq&tbYHM='.$xh.'&tbPSW='.$pw.'&ddlSF=%D1%A7%C9%FA&imgDL.x=31&imgDL.y=8';
+// $post_fields 	= '__VIEWSTATE=dDwtNjg3Njk1NzQ3O3Q8O2w8aTwxPjs%2BO2w8dDw7bDxpPDg%2BO2k8MTM%2BO2k8MTU%2BOz47bDx0PHA8O3A8bDxvbmNsaWNrOz47bDx3aW5kb3cuY2xvc2UoKVw7Oz4%2BPjs7Pjt0PHA8bDxWaXNpYmxlOz47bDxvPGY%2BOz4%2BOzs%2BO3Q8cDxsPFZpc2libGU7PjtsPG88Zj47Pj47Oz47Pj47Pj47bDxpbWdETDtpbWdUQzs%2BPvpW9bNHRO98aj%2BzEmn77FHqeOjK&tbYHM='.$xh.'&tbPSW='.$pw.'&ddlSF=%D1%A7%C9%FA&imgDL.x=28&imgDL.y=19';
 $submit_url 	= 'http://10.50.17.1/default3.aspx';//提价页面
-//$submit_url 	= 'http://10.50.17.2/default3.aspx';//提价页面
 
 $ch = curl_init($submit_url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -30,7 +30,7 @@ $geturl_xsxx = 'http://10.50.17.2/xsxx.aspx?xh='.$xh;//个人信息页面
 
  正则表达匹配信息
  preg_match("/<span id=\"xm\">(.*)<\/span>/",$string,$xm);
-
+ 
  $xh  学号
  $pw  密码
  $xm  姓名
@@ -41,10 +41,9 @@ $geturl_xsxx = 'http://10.50.17.2/xsxx.aspx?xh='.$xh;//个人信息页面
  $zyfx 专业方向
  $bjmc 班级名称
  $nj 年级
- */
+           */
 
 $geturl_xsxx = 'http://10.50.17.1/xsxx.aspx?xh='.$xh;//个人信息页面
-//$geturl_xsxx = 'http://10.50.17.2/xsxx.aspx?xh='.$xh;//个人信息页面
 
 
 
@@ -109,28 +108,28 @@ $nj=get_utf8_string($nj);
 if($xm){
 
 
-    $ret="学号----<font color=blue>".$xh."</font><br>密码----<font color=blue>".$pw."</font><br>年级----<font color=blue>".$nj."</font><br>姓名----<font color=blue>".$xm."</font><br>身份证号----<font color=blue>".$sfzh."</font><br>性别----<font color=blue>".$xb."</font><br>学院名称----<font color=blue>".$xymc."</font><br>专业名称----<font color=blue>".$zymc."</font><br>专业方向----<font color=blue>".$zyfx."</font><br>班级名称----<font color=blue>".$bjmc."</font><br>出生日期----<font color=blue>".$csrq."</font><br><br>";
-    echo $ret;
+$ret="学号----<font color=blue>".$xh."</font><br>密码----<font color=blue>".$pw."</font><br>年级----<font color=blue>".$nj."</font><br>姓名----<font color=blue>".$xm."</font><br>身份证号----<font color=blue>".$sfzh."</font><br>性别----<font color=blue>".$xb."</font><br>学院名称----<font color=blue>".$xymc."</font><br>专业名称----<font color=blue>".$zymc."</font><br>专业方向----<font color=blue>".$zyfx."</font><br>班级名称----<font color=blue>".$bjmc."</font><br>出生日期----<font color=blue>".$csrq."</font><br><br>";
+ echo $ret;
 
 
-    echo post_db($xh,$pw,$xm,$csrq,$xb,$xymc,$zymc,$zyfx,$bjmc,$nj);
-
+// echo post_db($xh,$pw,$xm,$csrq,$xb,$xymc,$zymc,$zyfx,$bjmc,$nj);
+ 
 
 }else{
-    echo get_utf8_string("获取失败...");
-}
+ echo get_utf8_string("获取失败...");
+ }
 function get_utf8_string($content) 
-{    	  
-    $encoding = mb_detect_encoding($content, array('ASCII','UTF-8','GB2312','GBK','BIG5'));  
-    return  mb_convert_encoding($content, 'utf-8', $encoding);
-}
+	{    	  
+		$encoding = mb_detect_encoding($content, array('ASCII','UTF-8','GB2312','GBK','BIG5'));  
+		return  mb_convert_encoding($content, 'utf-8', $encoding);
+	}
 function post_db($xh,$pw,$xm,$csrq,$xb,$xymc,$zymc,$zyfx,$bjmc,$nj) 
-{    	  
-    $api_url='http://ours.123nat.com:59832/helper/jwc/dbapi.php?xh='.$xh.'&pw='.$pw.'&xm='.$xm.'&csrq='.$csrq.'&xb='.$xb.'&xymc='.$xymc.'&zymc='.$zymc.'&zyfx='.$zyfx.'&bjmc='.$bjmc.'&nj='.$nj;
-    $ret_personinfo=file_get_contents($api_url);
-    return $ret_personinfo;
-
-}
-
-
+	{    	  
+		$api_url='http://ours.123nat.com:59832/jwc/dbapi.php?xh='.$xh.'&pw='.$pw.'&xm='.$xm.'&csrq='.$csrq.'&xb='.$xb.'&xymc='.$xymc.'&zymc='.$zymc.'&zyfx='.$zyfx.'&bjmc='.$bjmc.'&nj='.$nj;
+		$ret_personinfo=file_get_contents($api_url);
+		return $ret_personinfo;
+		
+	}
+	
+	
 ?>
